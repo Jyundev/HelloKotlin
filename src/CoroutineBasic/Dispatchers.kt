9572288f1f8a6195949e287fec3805b1603a3e55
jyunxxxxx <jyunxxxxx@gmail.com>
2021-11-23 19:02:06 +0900

    dispatcher

---
+++ src/CoroutineBasic/Dispatchers.kt
@@ -0,0 +1,43 @@
+package CoroutineBasic
+
+import kotlinx.coroutines.*
+
+fun log(msg: String) = CoroutineBasic.println("[${Thread.currentThread().name}] $msg")
+
+
+fun main() =
+    runBlocking<Unit> {
+        val a = async {
+            log("I'm computing a piece of the answer")
+            6
+        }
+        val b = async {
+            log("I'm computing another piece of the answer")
+            7
+        }
+
+        log("The answer is ${a.await() * b.await()}")
+
+    }
+
+
+fun dispatcherFunc() = runBlocking<Unit> {
+
+    launch {
+        println("main runBlocking : I'm working in thread ${Thread.currentThread().name}")
+    }
+    launch(Dispatchers.Unconfined) {
+        println("Unconfined : I'm working in thread ${Thread.currentThread().name}")
+    }
+    launch(Dispatchers.Default) {
+        println("Default : I'm working in thread ${Thread.currentThread().name}")
+    }
+    newSingleThreadContext("MyOwnThread").use {
+        launch(it) {
+            println("newSingleThreadContext : I'm working in thread ${Thread.currentThread().name}")
+        }
+    }
+
+}
+
+
